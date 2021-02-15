8f9e19ab96761711ae32482c41c024f868170017
Alexander Safonov <al.safonov@inbox.ru>
2021-02-16 01:38:01 +0300

    build: add setup.py for Python native build system
    Signed-off-by: Alexander Safonov <al.safonov@inbox.ru>

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('portscan',
+                    define_macros = [('MAJOR_VERSION', '1'),
+                                     ('MINOR_VERSION', '0')],
+                    include_dirs = ['/usr/local/include', 'submodule/portscanner/include'],
+                    libraries = ['portscanner'],
+                    library_dirs = ['/usr/local/lib', 'submodule/portscanner/build'],
+                    sources = ['python_portscan.c'])
+
+setup (name = 'Portscanner',
+       version = '1.0',
+       description = 'This is a demo package',
+       author = 'Alexander Safonov',
+       author_email = 'al.safonov@inbox.ru',
+       ext_modules = [module1])
